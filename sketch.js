315ab0e8163dcba35ad9c772f875e41a1b2226c2
Kabir <89661761+kabirbatraa@users.noreply.github.com>
2022-04-28 23:00:48 -0400

    cleaned generateNewTile code

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let tileWidth = 10;
+let tileWidth = 50;
 let newTile;
 
 let tileGrid;
@@ -15,48 +15,6 @@ function setup() {
   for (let i = 0; i < width/tileWidth; i++) {
     tileGrid.push([]);
     for (let j = 0; j < height/tileWidth; j++) {
-      // let remainingPositions = [0, 1, 2, 3];
-
-      // let newTileType = floor(random(2));
-      // newTileType = 0;
-
-      // if (i > 0) {
-      //   remainingPositions = remainingPositions.filter((value) => {
-      //     if (newTileType == 0) {
-      //       return !(tileGrid[i-1][j].rightAllowed() ^ TileT.rightAllowedFilter(value));
-      //       // return tileGrid[i-1][j].rightAllowed() ? TileT.rightAllowedFilter(value) : !TileT.rightAllowedFilter(value);
-      //     }
-      //     else {
-      //       return !(tileGrid[i-1][j].rightAllowed() ^ TileL.rightAllowedFilter(value));
-      //     }
-          
-      //   });
-      // }
-      // if (j > 0) {
-      //   remainingPositions = remainingPositions.filter((value) => {
-      //     if (newTileType == 0) {
-      //       return !(tileGrid[i][j-1].downAllowed() ^ TileT.downAllowedFilter(value));
-      //       // return tileGrid[i-1][j].rightAllowed() ? TileT.rightAllowedFilter(value) : !TileT.rightAllowedFilter(value);
-      //     }
-      //     else {
-      //       return !(tileGrid[i][j-1].downAllowed() ^ TileL.downAllowedFilter(value));
-      //     }
-          
-      //   });
-      // }
-      // if (remainingPositions.length == 0) {
-      //   tileGrid[i].push(new TileBlank(i*tileWidth, j*tileWidth));
-      // }
-      // else {
-      //   if (newTileType == 0) {
-      //     tileGrid[i].push(new TileT(remainingPositions[floor(random(0, remainingPositions.length))], i*tileWidth, j*tileWidth));
-      //   }
-      //   else {
-      //     tileGrid[i].push(new TileL(remainingPositions[floor(random(0, remainingPositions.length))], i*tileWidth, j*tileWidth));
-      //   }
-      // }
-      // // tileGrid[i].push(new TileT(0, i*tileWidth, j*tileWidth));
-      // tileGrid[i].push(generateNewTile(i, j));
       tileGrid[i].push(false);
     }
   }
@@ -91,47 +49,23 @@ function generateNewTile(i, j) {
 
   let remainingPositions = [0, 1, 2, 3];
 
-  let newTileType = floor(random(2));
-  // newTileType = 0;
+  let newTileType = floor(random(2)) ? TileT : TileL;
+  // newTileType = TileT;
 
   if (i > 0) {
     remainingPositions = remainingPositions.filter((value) => {
-      if (newTileType == 0) {
-        return !(tileGrid[i-1][j].rightAllowed() ^ TileT.rightAllowedFilter(value));
-        // return tileGrid[i-1][j].rightAllowed() ? TileT.rightAllowedFilter(value) : !TileT.rightAllowedFilter(value);
-      }
-      else {
-        return !(tileGrid[i-1][j].rightAllowed() ^ TileL.rightAllowedFilter(value));
-      }
-      
+      return !(tileGrid[i-1][j].rightAllowed() ^ newTileType.rightAllowedFilter(value));
     });
   }
   if (j > 0) {
     remainingPositions = remainingPositions.filter((value) => {
-      if (newTileType == 0) {
-        return !(tileGrid[i][j-1].downAllowed() ^ TileT.downAllowedFilter(value));
-        // return tileGrid[i-1][j].rightAllowed() ? TileT.rightAllowedFilter(value) : !TileT.rightAllowedFilter(value);
-      }
-      else {
-        return !(tileGrid[i][j-1].downAllowed() ^ TileL.downAllowedFilter(value));
-      }
-      
+      return !(tileGrid[i][j-1].downAllowed() ^ newTileType.downAllowedFilter(value));
     });
   }
   if (remainingPositions.length == 0) {
-    // tileGrid[i].push(new TileBlank(i*tileWidth, j*tileWidth));
     return new TileBlank(i*tileWidth, j*tileWidth);
   }
   else {
-    if (newTileType == 0) {
-      // tileGrid[i].push(new TileT(remainingPositions[floor(random(0, remainingPositions.length))], i*tileWidth, j*tileWidth));
-      return new TileT(remainingPositions[floor(random(0, remainingPositions.length))], i*tileWidth, j*tileWidth);
-    }
-    else {
-      // tileGrid[i].push(new TileL(remainingPositions[floor(random(0, remainingPositions.length))], i*tileWidth, j*tileWidth));
-      return new TileL(remainingPositions[floor(random(0, remainingPositions.length))], i*tileWidth, j*tileWidth);
-    }
+    return new newTileType(remainingPositions[floor(random(0, remainingPositions.length))], i*tileWidth, j*tileWidth);
   }
-  // tileGrid[i].push(new TileT(0, i*tileWidth, j*tileWidth));
-
 }
